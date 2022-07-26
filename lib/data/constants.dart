48c66d557f30834f55bec8dc1de612245b3adb7b
Daniel Murcia <daniel.murcia@remyrobotics.com>
2022-07-26 15:32:01 -0500

    .gitignore is now working

--- lib/data/constants.dart
+++
@@ -1,7 +0,0 @@
-class ApiConstants {
-  static String fetchUrl =
-      'https://od-api.oxforddictionaries.com:443/api/v2/entries/en-gb/';
-
-  static String apiAppId = 'YOUR_OXFORD_DICTIONARIES_APP_ID';
-  static String apiKey = 'YOUR_OXFORD_DICTIONARIES_API_KEY';
-}
